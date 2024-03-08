5f6acbcf5a423cdce3d9a8cf36642522e1fdea22
calangoxt <162368560+calangoxt@users.noreply.github.com>
2024-03-08 17:10:49 -0300

    Add files via upload

---
+++ aula3/js.js
@@ -0,0 +1,31 @@
+/*
+const eu = 'calango'
+const idade = 16
+
+const altura = 1.75
+const temperatura = -20 
+console.log(temperatura,)
+ let souUmboolean = true
+ souUmboolean = false
+ const nome = 'miguel'
+ const sobrenome = 'miranda'
+ const souEstudante = true
+ console.log('meu nome é',nome,sobrenome,'tenho',idade,'estuda',souEstudante,)
+ */
+ //typeof //este comando emprime o tipo desta vareavel ex: number, string, boolean.
+ /*const anime = 'black clover' 
+ const ep = 171
+ console.log(anime,'e tem',ep,'episodios ')*/
+ 
+ /*const numero = 10
+ const texto = numero.toString()
+ console.log(typeof numero)
+ console.log(typeof texto)
+
+ const numero2 = '10'
+ const texto2 = Number(numero2)
+ console.log(typeof numero2)
+ console.log(typeof texto2)*/
+ const n1 = 1 
+ const n2 = 2 
+ console.log(n1+n2)
