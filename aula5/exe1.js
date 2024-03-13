1271dbe3b27b317e92b5ba9a7d0abda698705348
calangoxt <162368560+calangoxt@users.noreply.github.com>
2024-03-13 17:22:34 -0300

    Add files via upload

---
+++ aula5/exe1.js
@@ -0,0 +1,7 @@
+let nome = 'calango'
+let idade = 16
+let id = 18
+let ano50 = 26
+let a = idade >= id
+let b = idade + ano50
+console.log('ola,quantos anos vc tem?' ,idade, 'vc é maior de idade?',a, 'quantos anos vc vai ter em 2050?',b,)
---
+++ aula5/operadorLogicoOU.js
@@ -0,0 +1,10 @@
+//Operador logico OU (ou) ||
+//retorna false se, e somente se,todos booleanos envolvidos tambem forem false 
+
+true || true//true
+false || true//true 
+true || false//true
+false || false//false
+
+
+
