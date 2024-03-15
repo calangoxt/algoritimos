48feb2b09d5496a13e0f9b82b067cb2072138d29
calangoxt <162368560+calangoxt@users.noreply.github.com>
2024-03-15 17:21:59 -0300

    Add files via upload

---
+++ aula6/condicionais.js
@@ -0,0 +1,7 @@
+let idade =8
+
+if(idade >= 18) {// frase so sera execultada se a variavel comprir o requisito
+    console.log('vc é maior de idade')
+} else {
+    console.log('vc é menor de idade')
+} 
---
+++ aula6/teste.js
@@ -0,0 +1,19 @@
+let goku =80000
+let frezza =80000
+
+if (goku > frezza) {
+    console.log('o poder é mais de 8mil!!!!')
+}else if (goku < frezza) {
+    console.log('vc é muito fraco')
+}else {
+console.log('nossos poderes sao iguais')
+}
+
+
+if (goku > frezza) {
+    console.log('o poder é mais de 8mil!!!!')
+}else if (goku < frezza) {
+    console.log('vc é muito fraco')
+}else {
+console.log('nossos poderes sao iguais')
+}
